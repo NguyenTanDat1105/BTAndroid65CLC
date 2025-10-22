dc99f2831b020c01f5beee7bf5dbd99f1f3914c1
Nguyễn Tấn Đạt <tandat2005nt@gmail.com>
2025-10-22 09:42:46 +0700

    ViDuIntent hoàn thiện

--- ViDuIntent/app/src/main/java/clc65/nguyentandat/viduintent/MainActivity.java
+++ ViDuIntent/app/src/main/java/clc65/nguyentandat/viduintent/MainActivity.java
@@ -1,6 +1,10 @@
 package clc65.nguyentandat.viduintent;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,12 +17,25 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        Button nutSend =  findViewById(R.id.btnSend);
+        nutSend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Code chuyển màn hình
+                // 1. Tạo Intent
+                Intent iChuyen = new Intent(MainActivity.this,SecondActivity.class);
+                // 2. Lấy dữ liệu vào iSend
+                    // 2.1. Lấy dữ liệu
+                    EditText edtHoTen =  findViewById(R.id.edtName);
+                    String data = edtHoTen.getText().toString();
+                    // 2.2. Gửi vào iSend, dùng putExtra(key/name, value)
+                    iChuyen.putExtra("ht", data);
+                    iChuyen.putExtra("copyright", "MCT");
+                // 3. Kích hoạt màn hình 2
+                startActivity(iChuyen);
+            }
         });
     }
 }
