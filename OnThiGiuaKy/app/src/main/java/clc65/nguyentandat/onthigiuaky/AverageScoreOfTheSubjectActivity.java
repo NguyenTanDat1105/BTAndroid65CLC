51382c03b4a294cfc9c5f6b19687cfbf2edb571a
Nguyễn Tấn Đạt <tandat2005nt@gmail.com>
2025-10-22 17:17:46 +0700

    OnThiGiuaKi Xong màn hình Main, Tính ĐTB môn, About Me

--- OnThiGiuaKy/app/src/main/java/clc65/nguyentandat/onthigiuaky/AverageScoreOfTheSubjectActivity.java
+++ OnThiGiuaKy/app/src/main/java/clc65/nguyentandat/onthigiuaky/AverageScoreOfTheSubjectActivity.java
@@ -1,6 +1,9 @@
 package clc65.nguyentandat.onthigiuaky;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +11,48 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class AverageScoreOfTheSubjectActivity extends AppCompatActivity {
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
 
+public class AverageScoreOfTheSubjectActivity extends AppCompatActivity {
+    Button btnTinhTB, btnBack;
+    TextInputLayout input_dgk, input_dck, output_dtbmon;
+    void TimDK() {
+        input_dgk = findViewById(R.id.input_gk);
+        input_dck = findViewById(R.id.input_ck);
+        btnTinhTB = findViewById(R.id.bt_tinh);
+        output_dtbmon = findViewById(R.id.output_kq);
+        btnBack = findViewById(R.id.btnBack);
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_average_score_of_the_subject);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        TimDK();
+
+        btnTinhTB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String strGiuaKy = input_dgk.getEditText().getText().toString();
+                String strCuoiKy = input_dck.getEditText().getText().toString();
+
+                Double DiemGiuaKy = Double.parseDouble(strGiuaKy);
+                Double DiemCuoiKy = Double.parseDouble(strCuoiKy);
+
+                Double DiemTBM = DiemGiuaKy * 0.5 + DiemCuoiKy * 0.5;
+
+                String diemTBM = String.valueOf(DiemTBM);
+
+                output_dtbmon.getEditText().setText(diemTBM);
+            }
+        });
+
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent iBack = new Intent (AverageScoreOfTheSubjectActivity.this, MainActivity.class);
+                startActivity(iBack);
+            }
         });
     }
 }
