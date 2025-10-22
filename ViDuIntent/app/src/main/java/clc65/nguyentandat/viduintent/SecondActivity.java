9c4f4ee3ddf65816fcf92fbc909d50685914b87a
Nguyễn Tấn Đạt <tandat2005nt@gmail.com>
2025-10-22 09:49:49 +0700

    ViDuIntent

--- ViDuIntent/app/src/main/java/clc65/nguyentandat/viduintent/SecondActivity.java
+++ ViDuIntent/app/src/main/java/clc65/nguyentandat/viduintent/SecondActivity.java
@@ -1,6 +1,9 @@
 package clc65.nguyentandat.viduintent;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -14,6 +17,23 @@ public class SecondActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+        // 1. Nhận về Intent
+        Intent iNhanDuoc = getIntent();
+        // 2. Bóc ra
+        String intNhanDuoc = iNhanDuoc.getStringExtra("ht");
+        // 3.Xử lý
+            // Set lên TextView
 
+
+
+        // Nút BACK
+        Button btnBack = findViewById(R.id.btnBack);
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent iQuayBack = new Intent(SecondActivity.this, MainActivity.class);
+                startActivity(iQuayBack);
+            }
+        });
     }
 }
